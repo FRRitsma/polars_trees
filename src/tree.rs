fafec8d40f0e58cb9e29524d8aba8dc378827d04
FRRitsma <folkert.ritsma@xomnia.com>
2025-03-26 11:14:20 +0100

    succesful rewrite of extract values

--- src/tree.rs
+++ src/tree.rs
@@ -2,11 +2,12 @@ use polars::prelude::Expr;
 
 
 struct Tree{
-    left_node: Some(Tree),
-    right_node: Some(Tree),
-    split_expression: Some(Expr),
+    left_node: Option<Tree>,
+    right_node: Option<Tree>,
+    split_expression: Option<Tree>,
     is_final: bool,
     depth: u8,
     minimal_sample_size: u16,
+    max_depth: u8,
 }
 
